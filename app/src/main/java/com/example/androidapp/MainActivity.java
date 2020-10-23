5a13a5f155bcce1bf0b93718f3e85d65df21054f
axntoniia <antonia.oberpriller12@gmail.com>
2020-10-23 12:06:03 +0200

    laufende Version Login

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.androidapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
